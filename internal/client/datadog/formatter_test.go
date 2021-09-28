057492f2043fde6198e4ed33e96e248d244c309a
NouemanKHAL <noueman.khal@gmail.com>
2021-09-28 10:55:42 +0200

    add test for new splitting logic

--- internal/client/datadog/formatter_test.go
+++ internal/client/datadog/formatter_test.go
@@ -1,6 +1,7 @@
 package datadog
 
 import (
+	"encoding/json"
 	"math"
 
 	"github.com/DataDog/datadog-firehose-nozzle/internal/metric"
@@ -12,11 +13,13 @@ import (
 
 var _ = Describe("Formatter", func() {
 	var (
-		formatter Formatter
+		formatter  Formatter
+		metricsMap metric.MetricsMap
 	)
 
 	BeforeEach(func() {
 		formatter = Formatter{gosteno.NewLogger("test")}
+		metricsMap = make(metric.MetricsMap)
 	})
 
 	It("does not return empty data", func() {
@@ -75,5 +78,41 @@ var _ = Describe("Formatter", func() {
 		result := formatter.Format("some-prefix", 1024, m)
 		Expect(string(helper.Decompress(result[0]))).To(ContainSubstring(`"metric":"bosh.healthmonitor.foo"`))
 		Expect(string(helper.Decompress(result[0]))).To(ContainSubstring(`"points":[[0,9.000000],[0,1.000000]]`))
+	})
+
+	It("properly assigns all values to split results when splitting", func() {
+		// first test a scenario where we're not splitting as there's just one point
+		m := make(map[metric.MetricKey]metric.MetricValue)
+		m[metric.MetricKey{Name: "a"}] = metric.MetricValue{
+			Points: []metric.Point{{Value: 9}},
+			Tags:   []string{"some:tag", "other:tag"},
+			Host:   "some.host",
+		}
+		result := formatter.Format("some-prefix.", 1, m)
+
+		Expect(result).To(HaveLen(1))
+
+		decompressed := helper.Decompress(result[0])
+		payload := Payload{}
+		err := json.Unmarshal(decompressed, &payload)
+		Expect(err).To(BeNil())
+		Expect(payload.Series).To(HaveLen(1))
+		s := payload.Series[0]
+		Expect(s.Metric).To(Equal("some-prefix.a"))
+		Expect(s.Points).To(Equal([]metric.Point{{Value: 9}}))
+		Expect(s.Type).To(Equal("gauge"))
+		Expect(s.Host).To(Equal("some.host"))
+		Expect(s.Tags).To(Equal([]string{"some:tag", "other:tag"}))
+
+		// now test a scenario where we're actually splitting metrics
+		for i := 0; i < 1000; i++ {
+			k, v := makeFakeMetric("metricName", 1000, uint64(i), defaultTags)
+			metricsMap.Add(k, v)
+		}
+
+		a, b := splitMetrics(metricsMap)
+
+		Expect(len(a)).To(BeNumerically("<=", len(metricsMap)))
+		Expect(len(b)).To(BeNumerically("<=", len(metricsMap)))
 	})
 })
