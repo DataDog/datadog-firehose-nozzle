c3b04baf9773f7927839e2563fad51c22b8ce02b
NouemanKHAL <noueman.khal@gmail.com>
2021-12-30 13:10:04 +0100

    add DCA client unit tests

--- internal/client/cloudfoundry/dca_client_test.go
+++ internal/client/cloudfoundry/dca_client_test.go
@@ -6,15 +6,23 @@ import (
 	. "github.com/onsi/gomega"
 
 	"github.com/DataDog/datadog-firehose-nozzle/internal/config"
-	"github.com/cloudfoundry-community/go-cfclient"
 	"github.com/cloudfoundry/gosteno"
 )
 
-func checkV3AppAttributes(app *cfclient.V3App, apiVersion int) {
-	Expect(app.GUID).To(Equal("6d254438-cc3b-44a6-b2e6-343ca92deb5f"))
-	Expect(app.Name).To(Equal("p-invitations-green"))
-	Expect(app.Relationships["space"].Data.GUID).To(Equal("417b893e-291e-48ec-94c7-7b2348604365"))
-	Expect(app.Metadata.Annotations).To(Equal(map[string]string{
+func checkDCAAppAttributes(app *CFApplication) {
+	Expect(app.GUID).To(Equal("a7bebd67-1991-4e9e-8d44-399acf2f13e8"))
+	Expect(app.Name).To(Equal("logs-backend-demo"))
+	Expect(app.SpaceGUID).To(Equal("68a6159c-cb4a-4f70-bb48-f2c24bd79c6b"))
+	Expect(app.SpaceName).To(Equal("system"))
+	Expect(app.OrgGUID).To(Equal("24d7098c-832b-4dfa-a4f1-950780ae92e9"))
+	Expect(app.OrgName).To(Equal("system"))
+	Expect(app.Instances).To(Equal(2))
+	Expect(app.Buildpacks).To(Equal([]string{"datadog_application_monitoring", "ruby_buildpack"}))
+	Expect(app.DiskQuota).To(Equal(1024))
+	Expect(app.TotalDiskQuota).To(Equal(2048))
+	Expect(app.Memory).To(Equal(256))
+	Expect(app.TotalMemory).To(Equal(512))
+	Expect(app.Annotations).To(Equal(map[string]string{
 		"app-space-org-annotation": "app-space-org-annotation-app-value",
 		"app-space-annotation":     "app-space-annotation-app-value",
 		"app-org-annotation":       "app-org-annotation-app-value",
@@ -23,7 +31,7 @@ func checkV3AppAttributes(app *cfclient.V3App, apiVersion int) {
 		"space-annotation":         "space-annotation-value",
 		"org-annotation":           "org-annotation-value",
 	}))
-	Expect(app.Metadata.Labels).To(Equal(map[string]string{
+	Expect(app.Labels).To(Equal(map[string]string{
 		"app-space-org-label": "app-space-org-label-app-value",
 		"app-space-label":     "app-space-label-app-value",
 		"app-org-label":       "app-org-label-app-value",
@@ -34,22 +42,13 @@ func checkV3AppAttributes(app *cfclient.V3App, apiVersion int) {
 	}))
 }
 
-func checkV3ProcessAttributes(process *cfclient.Process) {
-	Expect(process.GUID).To(Equal("6d254438-cc3b-44a6-b2e6-343ca92deb5f"))
-	Expect(process.Instances).To(Equal(1))
-	Expect(process.DiskInMB).To(Equal(1024))
-	Expect(process.MemoryInMB).To(Equal(256))
-}
-
-func checkV3SpaceAttributes(space *cfclient.V3Space) {
-	Expect(space.GUID).To(Equal("417b893e-291e-48ec-94c7-7b2348604365"))
-	Expect(space.Name).To(Equal("system"))
-	Expect(space.Relationships["organization"].Data.GUID).To(Equal("671557cf-edcd-49df-9863-ee14513d13c7"))
-}
-
-func checkV3OrgAttributes(org *cfclient.V3Organization) {
-	Expect(org.GUID).To(Equal("671557cf-edcd-49df-9863-ee14513d13c7"))
-	Expect(org.Name).To(Equal("system"))
+func checkVersionAttributes(version *Version) {
+	Expect(version.Major).To(BeEquivalentTo(1))
+	Expect(version.Minor).To(BeEquivalentTo(9))
+	Expect(version.Patch).To(BeEquivalentTo(5))
+	Expect(version.Pre).To(Equal("rc100"))
+	Expect(version.Meta).To(Equal("git.2598.2b50b49"))
+	Expect(version.Commit).To(Equal("2b50b49"))
 }
 
 var _ = Describe("DatadogClusterAgentClient", func() {
@@ -76,141 +75,31 @@ var _ = Describe("DatadogClusterAgentClient", func() {
 		Expect(err).To(BeNil())
 	}, 0)
 
-	Context("individual endpoint", func() {
-		It("with v3 spaces is retrieved correctly", func() {
-			res, err := fakeDCAClient.GetCFSpaces()
+	Context("GetVersion method", func() {
+		It("retrieves the cluster agent api version correctly", func() {
+			res, err := fakeDCAClient.GetVersion()
 			Expect(err).To(BeNil())
 			Expect(res).NotTo(BeNil())
-			Expect(len(res)).To(Equal(6))
-			checkV3SpaceAttributes(&res[0])
-		})
-
-		It("with v3 processes is retrieved correctly", func() {
-			res, err := fakeDCAClient.GetCFProcesses()
-			Expect(err).To(BeNil())
-			Expect(res).NotTo(BeNil())
-			Expect(len(res)).To(Equal(19))
-			checkV3ProcessAttributes(&res[0])
-		})
-
-		It("with v3 orgs is retrieved correctly", func() {
-			res, err := fakeDCAClient.GetCFOrgs()
-			Expect(err).To(BeNil())
-			Expect(res).NotTo(BeNil())
-			Expect(len(res)).To(Equal(2))
-			checkV3OrgAttributes(&res[0])
-		})
-
-		It("with v3 apps is retrieved correctly", func() {
-			res, err := fakeDCAClient.GetCFApps()
-			Expect(err).To(BeNil())
-			Expect(res).NotTo(BeNil())
-			Expect(len(res)).To(Equal(14))
+			checkVersionAttributes(&res)
 		})
 	})
 
-	// Context("getV2Applications method", func() {
-	// 	It("retrieves apps correctly", func() {
-	// 		fakeCfClient.NumWorkers = 1
-	// 		res, err := fakeCfClient.getV2Applications()
-	// 		Expect(err).To(BeNil())
-	// 		Expect(res).NotTo(BeNil())
-	// 		Expect(len(res)).To(Equal(45))
-	// 		checkAppAttributes(&res[0], 2)
+	Context("GetApplications method", func() {
+		It("retrieves all apps correctly from the cluster agent api", func() {
+			res, err := fakeDCAClient.GetApplications()
+			Expect(err).To(BeNil())
+			Expect(res).NotTo(BeNil())
+			Expect(len(res)).To(Equal(20))
+			checkDCAAppAttributes(&res[0])
+		})
+	})
 
-	// 		fakeCfClient.NumWorkers = 100 // More runners than pages
-	// 		res, err = fakeCfClient.getV2Applications()
-	// 		Expect(err).To(BeNil())
-	// 		Expect(res).NotTo(BeNil())
-	// 		Expect(len(res)).To(Equal(45))
-	// 		checkAppAttributes(&res[0], 2)
-
-	// 		fakeCfClient.NumWorkers = 3 // As many runners as pages
-	// 		res, err = fakeCfClient.getV2Applications()
-	// 		Expect(err).To(BeNil())
-	// 		Expect(res).NotTo(BeNil())
-	// 		Expect(len(res)).To(Equal(45))
-	// 		checkAppAttributes(&res[0], 2)
-	// 	})
-	// })
-
-	// Context("getV3Applications method", func() {
-	// 	It("retrieves apps correctly", func() {
-	// 		res, err := fakeCfClient.getV3Applications()
-	// 		Expect(err).To(BeNil())
-	// 		Expect(res).NotTo(BeNil())
-	// 		Expect(len(res)).To(Equal(14))
-	// 		checkAppAttributes(&res[0], 3)
-	// 	})
-	// })
-
-	// Context("GetApplications method", func() {
-	// 	It("retrieves apps correctly without specified API Version", func() {
-	// 		fakeCfClient.NumWorkers = 1
-	// 		res, err := fakeCfClient.GetApplications()
-	// 		Expect(err).To(BeNil())
-	// 		Expect(res).NotTo(BeNil())
-	// 		Expect(fakeCfClient.ApiVersion).To(Equal(3))
-	// 		Expect(len(res)).To(Equal(14))
-	// 		checkAppAttributes(&res[0], 3)
-
-	// 		fakeCfClient.NumWorkers = 100 // More runners than pages
-	// 		res, err = fakeCfClient.GetApplications()
-	// 		Expect(err).To(BeNil())
-	// 		Expect(res).NotTo(BeNil())
-	// 		Expect(fakeCfClient.ApiVersion).To(Equal(3))
-	// 		Expect(len(res)).To(Equal(14))
-	// 		checkAppAttributes(&res[0], 3)
-
-	// 		fakeCfClient.NumWorkers = 2 // As many runners as pages
-	// 		res, err = fakeCfClient.GetApplications()
-	// 		Expect(err).To(BeNil())
-	// 		Expect(res).NotTo(BeNil())
-	// 		Expect(fakeCfClient.ApiVersion).To(Equal(3))
-	// 		Expect(len(res)).To(Equal(14))
-	// 		checkAppAttributes(&res[0], 3)
-	// 	})
-
-	// 	It("retrieves apps correctly with explicitly specified v3 API version", func() {
-	// 		fakeCfClient.ApiVersion = 3
-	// 		res, err := fakeCfClient.GetApplications()
-	// 		Expect(err).To(BeNil())
-	// 		Expect(res).NotTo(BeNil())
-	// 		Expect(len(res)).To(Equal(14))
-	// 		checkAppAttributes(&res[0], 3)
-	// 	})
-
-	// 	It("retrieves apps correctly with explicitly specified v2 API version", func() {
-	// 		fakeCfClient.NumWorkers = 1
-	// 		fakeCfClient.ApiVersion = 2
-	// 		res, err := fakeCfClient.GetApplications()
-	// 		Expect(err).To(BeNil())
-	// 		Expect(res).NotTo(BeNil())
-	// 		Expect(len(res)).To(Equal(45))
-	// 		checkAppAttributes(&res[0], 2)
-
-	// 		fakeCfClient.NumWorkers = 100 // More runners than pages
-	// 		res, err = fakeCfClient.GetApplications()
-	// 		Expect(err).To(BeNil())
-	// 		Expect(res).NotTo(BeNil())
-	// 		Expect(len(res)).To(Equal(45))
-	// 		checkAppAttributes(&res[0], 2)
-
-	// 		fakeCfClient.NumWorkers = 3 // As many runners as pages
-	// 		res, err = fakeCfClient.GetApplications()
-	// 		Expect(err).To(BeNil())
-	// 		Expect(res).NotTo(BeNil())
-	// 		Expect(len(res)).To(Equal(45))
-	// 		checkAppAttributes(&res[0], 2)
-	// 	})
-	// })
-
-	// Context("GetApplication method", func() {
-	// 	It("retrieves app correctly", func() {
-	// 		res, err := fakeCfClient.GetApplication("6d254438-cc3b-44a6-b2e6-343ca92deb5f")
-	// 		Expect(err).To(BeNil())
-	// 		Expect(res).NotTo(BeNil())
-	// 		checkAppAttributes(res, 2)
-	// 	})
-	// })
+	Context("GetApplication method", func() {
+		It("retrieves a single app correctly from the cluster agent api", func() {
+			res, err := fakeDCAClient.GetApplication("a7bebd67-1991-4e9e-8d44-399acf2f13e8")
+			Expect(err).To(BeNil())
+			Expect(res).NotTo(BeNil())
+			checkDCAAppAttributes(res)
+		})
+	})
 })
