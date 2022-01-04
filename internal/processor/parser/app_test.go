89b695d3e1f3a5abb2a164f91067969dc988fe0d
NouemanKHAL <noueman.khal@gmail.com>
2022-01-04 14:03:15 +0100

    fix some unit tests

--- internal/processor/parser/app_test.go
+++ internal/processor/parser/app_test.go
@@ -25,14 +25,22 @@ var _ = Describe("AppMetrics", func() {
 		fakeCloudControllerAPI *FakeCloudControllerAPI
 		ccAPIURL               string
 		fakeCfClient           *cloudfoundry.CFClient
+		fakeClusterAgentAPI    *FakeClusterAgentAPI
+		dcaAPIURL              string
+		fakeDCAClient          *cloudfoundry.DCAClient
 	)
 
 	BeforeEach(func() {
 		log = gosteno.NewLogger("datadogclient test")
+
 		fakeCloudControllerAPI = NewFakeCloudControllerAPI("bearer", "123456789")
 		fakeCloudControllerAPI.Start()
-
 		ccAPIURL = fakeCloudControllerAPI.URL()
+
+		fakeClusterAgentAPI = NewFakeClusterAgentAPI("bearer", "123456789")
+		fakeClusterAgentAPI.Start()
+		dcaAPIURL = fakeClusterAgentAPI.URL()
+
 		config.NozzleConfig = config.Config{}
 		cfg := config.Config{
 			CloudControllerEndpoint: ccAPIURL,
@@ -40,9 +48,14 @@ var _ = Describe("AppMetrics", func() {
 			ClientSecret:            "123456789",
 			InsecureSSLSkipVerify:   true,
 			NumWorkers:              5,
+			DCAUrl:                  dcaAPIURL,
+			DCAToken:                "123456789",
+			DCAEnabled:              true,
 		}
 		var err error
 		fakeCfClient, err = cloudfoundry.NewClient(&cfg, log)
+		Expect(err).To(BeNil())
+		fakeDCAClient, err = cloudfoundry.NewDCAClient(&cfg, log)
 		Expect(err).To(BeNil())
 	}, 0)
 
@@ -52,15 +65,29 @@ var _ = Describe("AppMetrics", func() {
 			Expect(err).NotTo(BeNil())
 		})
 
-		It("generates it properly when it can connect", func() {
+		It("generates it properly when it can connect with cloudfoundry client", func() {
 			a, err := NewAppParser(fakeCfClient, nil, 5, 10, log, []string{}, "")
+			Expect(err).To(BeNil())
+			Expect(a).NotTo(BeNil())
+		})
+
+		It("generates it properly when it can connect with cluster agent client", func() {
+			a, err := NewAppParser(nil, fakeDCAClient, 5, 10, log, []string{}, "")
 			Expect(err).To(BeNil())
 			Expect(a).NotTo(BeNil())
 		})
 	})
 
 	Context("cache warmup", func() {
-		It("requests all the apps directly at startup", func() {
+		It("requests all the apps directly at startup using dca client if it is enabled", func() {
+			a, err := NewAppParser(nil, fakeDCAClient, 5, 999, log, []string{}, "")
+			Expect(err).To(BeNil())
+			Expect(a).NotTo(BeNil())
+			Eventually(a.AppCache.IsWarmedUp).Should(BeTrue())
+			Expect(len(a.AppCache.apps)).To(Equal(21))
+		})
+
+		It("requests all the apps directly at startup using cloudfoundry client only when DCA is disabled", func() {
 			a, err := NewAppParser(fakeCfClient, nil, 5, 999, log, []string{}, "")
 			Expect(err).To(BeNil())
 			Expect(a).NotTo(BeNil())
@@ -68,9 +95,20 @@ var _ = Describe("AppMetrics", func() {
 			Expect(len(a.AppCache.apps)).To(Equal(14))
 		})
 
-		It("does not block while warming cache", func() {
+		It("does not block while warming cache using cloudfoundry client", func() {
 			fakeCloudControllerAPI.RequestTime = 100
 			a, err := NewAppParser(fakeCfClient, nil, 5, 999, log, []string{}, "")
+			// Assertions are done while cache is warming up in the background
+			Expect(err).To(BeNil())
+			Expect(a).NotTo(BeNil())
+			Expect(a.AppCache.IsWarmedUp()).To(BeFalse())
+			// Eventually, the cache is ready
+			Eventually(a.AppCache.IsWarmedUp, 10*time.Second).Should(BeTrue())
+		})
+
+		It("does not block while warming cache using cluster agent client", func() {
+			fakeClusterAgentAPI.RequestTime = 100
+			a, err := NewAppParser(nil, fakeDCAClient, 5, 999, log, []string{}, "")
 			// Assertions are done while cache is warming up in the background
 			Expect(err).To(BeNil())
 			Expect(a).NotTo(BeNil())
@@ -81,7 +119,7 @@ var _ = Describe("AppMetrics", func() {
 	})
 
 	Context("app metrics test", func() {
-		It("tries to get it from the cloud controller when not in the cache", func() {
+		It("tries to get it from the cloud controller when not in the cache and cluster agent is disabled", func() {
 			a, _ := NewAppParser(fakeCfClient, nil, 5, 10, log, []string{}, "")
 			var req *http.Request
 			// Wait for cache warmup to finish
@@ -92,8 +130,29 @@ var _ = Describe("AppMetrics", func() {
 			Expect(req.URL.Path).To(Equal("/v2/apps/app-5"))
 		})
 
-		It("grabs from the cache when it present", func() {
+		It("tries to get it from the cluster agent api when not in the cache", func() {
+			a, _ := NewAppParser(nil, fakeDCAClient, 5, 10, log, []string{}, "")
+			var req *http.Request
+			// Wait for cache warmup to finish
+			Eventually(fakeClusterAgentAPI.ReceivedRequests).ShouldNot(Receive())
+			_, err := a.getAppData("app-5")
+			Expect(err).ToNot(BeNil()) // error expected because fake API won't return an app, so unmarshalling will fail
+			Eventually(fakeClusterAgentAPI.ReceivedRequests).Should(Receive(&req))
+			Expect(req.URL.Path).To(Equal("/api/v1/cf/apps/app-5"))
+		})
+
+		It("grabs from the cache when it present using cloudfoundry client", func() {
 			a, _ := NewAppParser(fakeCfClient, nil, 5, 10, log, []string{}, "")
+			Eventually(a.AppCache.IsWarmedUp).Should(BeTrue())
+			// 6116f9ec-2bd6-4dd6-b7fe-a1b6acf6662a corresponds to hello-datadog-cf-ruby-dev
+			Expect(a.AppCache.apps).To(HaveKey("6116f9ec-2bd6-4dd6-b7fe-a1b6acf6662a"))
+			app, err := a.getAppData("6116f9ec-2bd6-4dd6-b7fe-a1b6acf6662a")
+			Expect(err).To(BeNil())
+			Expect(app).NotTo(BeNil())
+		})
+
+		It("grabs from the cache when it present using dca client", func() {
+			a, _ := NewAppParser(nil, fakeDCAClient, 5, 10, log, []string{}, "")
 			Eventually(a.AppCache.IsWarmedUp).Should(BeTrue())
 			// 6116f9ec-2bd6-4dd6-b7fe-a1b6acf6662a corresponds to hello-datadog-cf-ruby-dev
 			Expect(a.AppCache.apps).To(HaveKey("6116f9ec-2bd6-4dd6-b7fe-a1b6acf6662a"))
@@ -104,7 +163,7 @@ var _ = Describe("AppMetrics", func() {
 	})
 
 	Context("metric evaluation test", func() {
-		It("parses an event properly and adds metadata if configured", func() {
+		It("parses an event properly and adds metadata if configured using cloudfoundry client", func() {
 			config.NozzleConfig.EnableMetadataCollection = true
 			config.NozzleConfig.MetadataKeysBlacklist = []*regexp.Regexp{regexp.MustCompile("blacklisted.*")}
 			a, err := NewAppParser(fakeCfClient, nil, 5, 10, log, []string{}, "env_name")
@@ -191,6 +250,95 @@ var _ = Describe("AppMetrics", func() {
 				Expect(metric.MetricValue.Tags).ToNot(ContainElement("label/blacklisted_key:bar"))
 			}
 		})
+
+		It("parses an event properly and adds metadata if configured using cluster agent client", func() {
+			config.NozzleConfig.EnableMetadataCollection = true
+			config.NozzleConfig.MetadataKeysBlacklist = []*regexp.Regexp{regexp.MustCompile("blacklisted.*")}
+			a, err := NewAppParser(nil, fakeDCAClient, 5, 10, log, []string{}, "env_name")
+			Expect(err).To(BeNil())
+			Eventually(a.AppCache.IsWarmedUp).Should(BeTrue())
+
+			event := &loggregator_v2.Envelope{
+				Timestamp:  1000000000,
+				SourceId:   "6116f9ec-2bd6-4dd6-b7fe-a1b6acf6662a",
+				InstanceId: "4",
+				Tags: map[string]string{
+					"origin":     "test-origin",
+					"deployment": "deployment-name",
+					"job":        "doppler",
+					"index":      "1",
+					"ip":         "10.0.1.2",
+				},
+				Message: &loggregator_v2.Envelope_Gauge{
+					Gauge: &loggregator_v2.Gauge{
+						Metrics: map[string]*loggregator_v2.GaugeValue{
+							"cpu": {
+								Unit:  "gauge",
+								Value: float64(1),
+							},
+							"memory": {
+								Unit:  "gauge",
+								Value: float64(1),
+							},
+							"disk": {
+								Unit:  "gauge",
+								Value: float64(1),
+							},
+							"memory_quota": {
+								Unit:  "gauge",
+								Value: float64(1),
+							},
+							"disk_quota": {
+								Unit:  "gauge",
+								Value: float64(1),
+							},
+						},
+					},
+				},
+			}
+
+			metrics, err := a.Parse(event)
+
+			Expect(err).To(BeNil())
+			Expect(metrics).To(HaveLen(10))
+
+			Expect(metrics).To(ContainMetric("app.disk.configured"))
+			Expect(metrics).To(ContainMetric("app.disk.provisioned"))
+			Expect(metrics).To(ContainMetric("app.memory.configured"))
+			Expect(metrics).To(ContainMetric("app.memory.provisioned"))
+			Expect(metrics).To(ContainMetric("app.instances"))
+			Expect(metrics).To(ContainMetric("app.cpu.pct"))
+			Expect(metrics).To(ContainMetric("app.disk.used"))
+			Expect(metrics).To(ContainMetric("app.disk.quota"))
+			Expect(metrics).To(ContainMetric("app.memory.used"))
+			Expect(metrics).To(ContainMetric("app.memory.quota"))
+
+			for _, metric := range metrics {
+				Expect(metric.MetricValue.Tags).To(ContainElement("app_name:hello-datadog-cf-ruby-dev"))
+				Expect(metric.MetricValue.Tags).To(ContainElement("guid:6116f9ec-2bd6-4dd6-b7fe-a1b6acf6662a"))
+				Expect(metric.MetricValue.Tags).To(ContainElement("env:env_name"))
+				Expect(metric.MetricValue.Tags).To(ContainElement("source_id:6116f9ec-2bd6-4dd6-b7fe-a1b6acf6662a"))
+				Expect(metric.MetricValue.Tags).To(ContainElement("label/app-space-org-label:app-space-org-label-app-value"))
+				Expect(metric.MetricValue.Tags).To(ContainElement("label/app-space-label:app-space-label-app-value"))
+				Expect(metric.MetricValue.Tags).To(ContainElement("label/app-org-label:app-org-label-app-value"))
+				Expect(metric.MetricValue.Tags).To(ContainElement("label/app-label:app-label-value"))
+				// Expect(metric.MetricValue.Tags).To(ContainElement("label/space-org-label:space-org-label-space-value"))
+				// Expect(metric.MetricValue.Tags).To(ContainElement("label/space-label:space-label-value"))
+				// Expect(metric.MetricValue.Tags).To(ContainElement("label/org-label:org-label-value"))
+				Expect(metric.MetricValue.Tags).To(ContainElement("annotation/app-space-org-annotation:app-space-org-annotation-app-value"))
+				Expect(metric.MetricValue.Tags).To(ContainElement("annotation/app-space-annotation:app-space-annotation-app-value"))
+				Expect(metric.MetricValue.Tags).To(ContainElement("annotation/app-org-annotation:app-org-annotation-app-value"))
+				Expect(metric.MetricValue.Tags).To(ContainElement("annotation/app-annotation:app-annotation-value"))
+				// Expect(metric.MetricValue.Tags).To(ContainElement("annotation/space-org-annotation:space-org-annotation-space-value"))
+				// Expect(metric.MetricValue.Tags).To(ContainElement("annotation/space-annotation:space-annotation-value"))
+				// Expect(metric.MetricValue.Tags).To(ContainElement("annotation/org-annotation:org-annotation-value"))
+				Expect(metric.MetricValue.Tags).To(ContainElement("auto-annotation-tag:auto-annotation-tag-value"))
+				Expect(metric.MetricValue.Tags).To(ContainElement("auto-label-tag:auto-label-tag-value"))
+				Expect(metric.MetricValue.Tags).ToNot(ContainElement("annotation/blacklisted_key:foo"))
+				Expect(metric.MetricValue.Tags).ToNot(ContainElement("label/blacklisted_key:bar"))
+			}
+		})
+
 		It("parses an event properly and doesn't add metadata if not configured, except for autodiscovery tags", func() {
 			a, err := NewAppParser(fakeCfClient, nil, 5, 10, log, []string{}, "env_name")
 			Expect(err).To(BeNil())
