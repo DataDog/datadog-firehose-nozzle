04d5cb0832daf7c5c0d76549f9211407cb405358
NouemanKHAL <noueman.khal@gmail.com>
2022-02-11 17:46:11 +0100

    fix retry mechanism

--- internal/client/cloudfoundry/dca_client.go
+++ internal/client/cloudfoundry/dca_client.go
@@ -56,22 +56,24 @@ func NewDCAClient(config *config.Config, logger *gosteno.Logger) (*DCAClient, er
 	dcaClient.clusterAgentAPIClient.Timeout = 2 * time.Second
 
 	// Validate the cluster agent client by checking the version
-	ticker := time.NewTicker(5 * time.Second)
-	nbrAttempts := 10
-
-RetryLoop:
-	for {
-		select {
-		case <-ticker.C:
-			dcaClient.ClusterAgentVersion, err = dcaClient.GetVersion()
-			if err == nil {
-				break RetryLoop
-			} else {
-				logger.Warnf("Unsuccessful attempt to connect to the Datadog Cluster Agent: %s", err)
-				nbrAttempts -= 1
-				if nbrAttempts == 0 {
-					logger.Errorf("Could not to connect to the Datadog Cluster Agent: %s", err)
-					os.Exit(1)
+	dcaClient.ClusterAgentVersion, err = dcaClient.GetVersion()
+	if err != nil {
+		ticker := time.NewTicker(5 * time.Second)
+		nbrAttempts := 10
+	RetryLoop:
+		for {
+			select {
+			case <-ticker.C:
+				dcaClient.ClusterAgentVersion, err = dcaClient.GetVersion()
+				if err == nil {
+					break RetryLoop
+				} else {
+					logger.Warnf("Unsuccessful attempt to connect to the Datadog Cluster Agent: %s", err)
+					nbrAttempts -= 1
+					if nbrAttempts == 0 {
+						logger.Errorf("Could not to connect to the Datadog Cluster Agent: %s", err)
+						os.Exit(1)
+					}
 				}
 			}
 		}
