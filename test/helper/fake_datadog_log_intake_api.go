2b1e4f9e242a3bfc6c24a7e14a0e8fe971f0631d
NouemanKHAL <noueman.khal@gmail.com>
2022-07-08 01:41:49 +0200

    update internal metrics tests

---
+++ test/helper/fake_datadog_log_intake_api.go
@@ -0,0 +1,41 @@
+package helper
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+)
+
+type FakeDatadogLogIntakeAPI struct {
+	server           *httptest.Server
+	ReceivedContents chan []byte
+}
+
+func NewFakeDatadogLogIntakeAPI() *FakeDatadogLogIntakeAPI {
+	return &FakeDatadogLogIntakeAPI{
+		ReceivedContents: make(chan []byte, 100),
+	}
+}
+
+func (f *FakeDatadogLogIntakeAPI) Start() {
+	f.server = httptest.NewUnstartedServer(f)
+	f.server.Start()
+}
+
+func (f *FakeDatadogLogIntakeAPI) Close() {
+	f.server.CloseClientConnections()
+	f.server.Close()
+}
+
+func (f *FakeDatadogLogIntakeAPI) URL() string {
+	return f.server.URL
+}
+
+func (f *FakeDatadogLogIntakeAPI) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
+	contents, _ := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+
+	go func() {
+		f.ReceivedContents <- contents
+	}()
+}
